c79b7047c5849f44c6d4bc391d196284838923f9
Alice Denise Miller <millera2@millera2.local>
2020-09-07 12:56:49 -0400

    Grinding

---
+++ CS-111/.ipynb_checkpoints/adt_turtle-checkpoint.py
@@ -0,0 +1,17 @@
+
+import turtle
+
+bob = turtle.Turtle()     #Using the "constructor function"  Turtle().  
+jill = turtle.Turtle()    # Creating a new turtle object.
+
+
+    
+def makeSquare(sidelength):
+    for i in range(4):
+        bob.forward(sidelength)
+        bob.left(90)
+    
+
+
+makeSquare()
+
