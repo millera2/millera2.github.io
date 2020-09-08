efa1b22faf0152b408fced17f774235e677e8a89
Alice Denise Miller <millera2@millera2.local>
2020-09-08 06:58:19 -0400

    Grinding

--- CS-111/.ipynb_checkpoints/adt_turtle-checkpoint.py
+++ CS-111/.ipynb_checkpoints/adt_turtle-checkpoint.py
@@ -5,13 +5,20 @@ bob = turtle.Turtle()     #Using the "constructor function"  Turtle().
 jill = turtle.Turtle()    # Creating a new turtle object.
 
 
-    
+screen = bob.getscreen()
+
 def makeSquare(sidelength):
     for i in range(4):
         bob.forward(sidelength)
         bob.left(90)
     
+    
+def moveToClick(x,y):
+    bob.goto()
+    
+bob.onclick(moveToClick)
+    
 
 
-makeSquare()
+turtle.mainloop()
 
