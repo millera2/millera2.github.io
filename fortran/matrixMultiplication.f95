2473990fb7f3f8215d7cb3a2d1f03882dc6ffe3b
Alice Denise Miller <millera2@millera2.local>
2020-09-21 18:27:36 -0400

    Grinding

--- fortran/matrixMultiplication.f95
+++ fortran/matrixMultiplication.f95
@@ -20,7 +20,7 @@ end do
 print *, "Matrix A is:"
 
 do i = 1,5
-  print "(5F8.2)", a(i,1), a(i,2), a(i,3), a(i,4), a(i,5)
+  print "(5F10.2)", a(i,1), a(i,2), a(i,3), a(i,4), a(i,5)
 end do
 
 print *, "------"
@@ -29,7 +29,7 @@ print *, "------"
 print *, "Matrix B is:"
 
 do i = 1,5
-   print "(5F8.2)", b(i,1), b(i,2), b(i,3), b(i,4), b(i,5)
+   print "(5F10.2)", b(i,1), b(i,2), b(i,3), b(i,4), b(i,5)
 end do
 
 print *, "------"
@@ -45,7 +45,7 @@ end do
 print *, "Matrix C = A*B is:"
 
 do i = 1,5
-   print "(5F8.2)", c(i,1), c(i,2), c(i,3), c(i,4), c(i,5)
+   print "(5F10.2)", c(i,1), c(i,2), c(i,3), c(i,4), c(i,5)
 end do
 
 
